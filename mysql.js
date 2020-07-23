917d41e0e20cd17ba836cc6662c65b6a99180a26
rashmiranganath <rashmi18@navgurukul.org>
2020-07-23 16:10:11 +0530

    updated

--- mysql.js
+++ mysql.js
@@ -1,13 +1,14 @@
 console.log("mysql")
 require('dotenv').config()
 
+
 const connection = {
     client: 'mysql',
     connection: {
-        host : process.env.db_host,
-        user: process.env.db_user ,
-        password : process.env.db_password,
-        database : process.env.db
+        host : process.env.DB_HOST ,
+        user: process.env.DB_USER ,
+        password : process.env.DB_PASS,
+        database : process.env.DB_NAME
     }
 }
 
